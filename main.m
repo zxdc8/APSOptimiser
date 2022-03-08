c30ab0676438f0805c5e3f26c0a9e0ddcdd72736
zxdc8 <oliver.c.cairns@gmail.com>
2022-03-08 12:15:10 +0000

    Modified fmincon and limits

--- main.m
+++ main.m
@@ -12,8 +12,13 @@ fclose(ifile);
 
 % This isn't working yet but i cba to find out why, will probably use a
 % better method anyway
+%
 x0=[10 5 5 10 10 20 5]
+%x0=[15 2]
+LB=[5 2 2 5 2 4 2] 
+UB=[20 15 15 30 20 40 10]
 
-[X,J]=fmincon(@(x)aeromodule(x),x0,[],[])
+options = optimoptions('fmincon','Algorithm','active-set','Display','iter-detailed','FiniteDifferenceStepSize',0.5,'FunctionTolerance',1e-10);
+[X,J]=fmincon(@aeromodule,x0,[],[],[],[],LB,UB,[],options)
 
 fclose('all');
