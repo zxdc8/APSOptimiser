31b26d175fe91f3e0c5095d00fa8c0f8239826d5
do18875 <100576020+do18875@users.noreply.github.com>
2022-03-14 11:32:47 +0000

    a

--- Functions/Performance/Engine/Release R1.6b/Data/Aircraft Parameters/BWB.m
+++ Functions/Performance/Engine/Release R1.6b/Data/Aircraft Parameters/BWB.m
@@ -11,13 +11,13 @@ Par.g                      = 9.80665;   % Gravity [m/s^2]
 Par.rho0                   = 1.225;     % Sea level density [kg/m^3]
 
 % Design Parameters
-Par.S                      = 65;     % Wing area [m^2] (DP)
-Par.Range_req              = 3000;     % Required design range [nm] (DP)
-Par.PLmax                  = 7600;     % Max payload [kg] (DP)
-Par.MFC                    = 845;       % Max Fuel capacity [kg] (DP)
-Par.MTOM                   = 26604;    % Max Take Off Mass [kg] (DP)
-Par.Airframe               = 18250;    % Operating Mass Empty [kg] (DP)
-Par.PL_req                 = 7600;     % Design Payload [kg] (DP)
+Par.S                      = 376.4;     % Wing area [m^2] (DP)
+Par.Range_req              = 3500;     % Required design range [nm] (DP)
+Par.PLmax                  = 45000;     % Max payload [kg] (DP)
+Par.MFC                    = 80000;       % Max Fuel capacity [kg] (DP)
+Par.MTOM                   = 230000;    % Max Take Off Mass [kg] (DP)
+Par.Airframe               = 130000;    % Operating Mass Empty [kg] (DP)
+Par.PL_req                 = 30000;     % Design Payload [kg] (DP)
 
 % Engine parmetars
 Par.EngFileName            = 'HydrogenTurbojet'; % Aircraft type for engine data selection
@@ -26,12 +26,12 @@ Par.BPR                    = 12;         % By pass ratio
 Par.TakeoffStaticThrust_lb = 30000;     % Take off thrust [lb] (DP)
 
 % Aerodynamics data From typical values for large turboprop 
-% Introduction to Aircraft Design, University of Liege)
-Par.CD0_Clean              = 0.0296;     % Zero Lift Drag Coef [-]
-Par.CD0_LD                 = 0.0920;     % Zero Lift Drag Coef - flaps down [-]
-Par.K_Clean                = 0.0259;     % Induced Drag factor - clean [-]
-Par.K_LD                   = 0.0303;     % Induced Drag factor - flaps down [-]
-Par.CLmax_LD               = 2.78;      % Max Lift Coef - flaps down [-]
+% Aerodynamics data
+Par.CD0_Clean              = 0.015;     % Zero Lift Drag Coef [-] (DP)
+Par.CD0_LD                 = 0.055;     % Zero Lift Drag Coef - flaps down [-] (DP)
+Par.K_Clean                = 0.05 ;     % Induced Drag factor - clean [-] (DP)
+Par.K_LD                   = 0.05 ;     % Induced Drag factor - flaps down [-] (DP)
+Par.CLmax_LD               = 2.5 ;      % Max Lift Coef - flaps down [-] (DP)
 Par.DragRise               = 0;         % Flag to switch drag rise in the drag polar: 1 = Yes, 0 = No
 Par.DragRise_Func          = 'DragRise_B777_AJenk'; % Name of Drag Rise Funcitn, e.g.'DragRise_B777_AJenk';
 
@@ -42,25 +42,25 @@ Par.Time_ContCruise        = 45   ;     % Time Continuous Cruise [min]
 Par.Time_Hold              = 0   ;     % Time Hold [min] NO HOLD accounted
 Par.Time_Taxiin            = 5   ;      % Time Taxi-in[min]
 
-Par.CAS_Hold               = 160 ;    % Speed CAS in Hold [kts]
+Par.CAS_Hold               = 225.5 ;    % Speed CAS in Hold [kts]
 Par.CAS_Mis_ClimbLow       = 250   ;    % Speed CAS in Climb 0 - 10000 ft [kts]
-Par.CAS_Mis_ClimbHigh      = 250   ;    % Speed CAS in Climb above 10000 ft [kts]
+Par.CAS_Mis_ClimbHigh      = 300   ;    % Speed CAS in Climb above 10000 ft [kts]
 Par.CAS_Mis_DescentLow     = 250   ;    % Speed CAS Descent - Low [kts]
-Par.CAS_Mis_DescentHigh    = 250   ;    % Speed CAS Descent - High [kts]
+Par.CAS_Mis_DescentHigh    = 300   ;    % Speed CAS Descent - High [kts]
 Par.CAS_Div_ClimbLow       = 250   ;    % Speed CAS Div Climb - Low [kts]
-Par.CAS_Div_ClimbHigh      = 250   ;    % Speed CAS Div Climb - High [kts]
+Par.CAS_Div_ClimbHigh      = 300   ;    % Speed CAS Div Climb - High [kts]
 Par.CAS_Div_DescentLow     = 250   ;    % Speed CAS Div Descent - Low [kts]
-Par.CAS_Div_DescentHigh    = 250   ;    % Speed CAS Div Descent - High [kts]
+Par.CAS_Div_DescentHigh    = 300   ;    % Speed CAS Div Descent - High [kts]
 
 Par.Mach_Cruise            = 0.45;      % Mach number cruise [-]
 Par.Mach_Diversion         = Par.Mach_Cruise;      % Mach number Div cruise [-]
-Par.Mach_Mis_Climb         = Par.Mach_Cruise-0.01;      % Mach number Climb high alt [-]
-Par.Mach_Mis_Descent       = Par.Mach_Cruise-0.01;      % Mach number Decent High [-]
-Par.Mach_Div_Climb         = Par.Mach_Cruise-0.01;      % Mach number Div Climb [-]
-Par.Mach_Div_Descent       = Par.Mach_Cruise-0.01;      % Mach number Div Descent [-]
+Par.Mach_Mis_Climb         = Par.Mach_Cruise-0.02;      % Mach number Climb high alt [-]
+Par.Mach_Mis_Descent       = Par.Mach_Cruise-0.02;      % Mach number Decent High [-]
+Par.Mach_Div_Climb         = Par.Mach_Cruise-0.02;      % Mach number Div Climb [-]
+Par.Mach_Div_Descent       = Par.Mach_Cruise-0.02;      % Mach number Div Descent [-]
 
 Par.Alt_Cruise             = 35000;     % Cruise Alt [ft]
-Par.Alt_Diversion          = 20000;     % Div Cruise Alt [ft]
+Par.Alt_Diversion          = 25000;     % Div Cruise Alt [ft]
 Par.Alt_Hold               = 1500;      % Hold Alt [ft]
 Par.Alt_approach           = 1500;      % Approach Alt [ft]
 Par.Alt_ClimbDescent_min   = 1500;      % Min Climb/Descent Alt [ft]
@@ -81,7 +81,7 @@ Par.Tol_PercentPolicy      = 0.01;      % Computation Tolerance for reserve iter
 Par.dM_max                 = 0.2;       % Max allowed difference for Mach number in engine data
 Par.dh_max                 = 5000;      % Max allowed difference for Alltitide in engine data
 
-Par.interp_method          = 'linear';  % 'spline' or 'linear'
+Par.interp_method          = 'spline';  % 'spline' or 'linear'
 
 end
 
