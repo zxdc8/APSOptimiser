685d64c50fe3d82fa53cc60cfc2727d100d1dd3b
do18875 <100576020+do18875@users.noreply.github.com>
2022-03-17 12:18:28 +0000

    help what am i doing

--- Functions/constraints.m
+++ Functions/constraints.m
@@ -4,7 +4,6 @@ function [C, Ceq]= constraints(x)
 %Calculate Wing Area
 [S,X,Z,dih]=DesignToSXZ(x);
 
-
 A=0.5*(S(1)+S(2))*(Z(2));
 At=sum(A)*2;
 
@@ -28,11 +27,6 @@ VFuel=1000;
 
 %Total Volume
 VT=VFuel+VpasT;
-=======
-[VFus, VW, VP]=AreasVolumes(S,X,Z,dih);
-
-%Estimate Volume of Fuel- RICKY!
-VFuel=50;
 
 %Requires function for Mach, Alt, S, CD0, k
 
@@ -49,11 +43,7 @@ VFuel=50;
 C(1)=S(2)-S(1);
 C(2)=S(3)-S(2);
 
-C(3)=S(2)+X(2)-72;  %Length Constraint
-C(4)=S(3)+X(3)-72;  %Length Constraint
-
 %Equality Constraints
-Ceq(1)=VFuel-VW;     %Fuel Volume
-Ceq(2)=VP-VFus;      %Pax Volume
-Ceq(3)=x(6)+x(7)-40; %Wingspan
+Ceq=-VT+V;
+
 end
