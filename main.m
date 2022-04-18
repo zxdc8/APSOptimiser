fb20280708f9ea12f5654e5b8290f1089bc534f9
do18875 <100576020+do18875@users.noreply.github.com>
2022-04-18 14:54:15 +0100

    Parallelised for faster perf code

--- main.m
+++ main.m
@@ -31,7 +31,7 @@ UB=[72 72 72 72 72 35 40];
 objFun=@(x)objective(x);
 
 %Define solver options 
-options = optimoptions('fmincon','Algorithm','active-set','Display','iter-detailed','FiniteDifferenceStepSize',2,'FunctionTolerance',5e-7,'StepTolerance',1e-7,'PlotFcns',@optimplotfval);
+options = optimoptions('fmincon','Algorithm','active-set','Display','iter-detailed','FiniteDifferenceStepSize',2,'FunctionTolerance',5e-7,'StepTolerance',1e-7,'PlotFcns',@optimplotfval,'UseParallel',true);
 
 %Run Optimisation
 [X,J]=fmincon(objFun,x0,[],[],[],[],LB,UB,@constraints,options)   
