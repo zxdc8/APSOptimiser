5bf2846677f44f288183b342aecd7d944c3a5394
do18875 <100576020+do18875@users.noreply.github.com>
2022-04-21 15:15:20 +0100

    Testing GA

---
+++ Functions/objectiveGA.m
@@ -0,0 +1,66 @@
+%////////OBJECTIVE FUNCTION/////////
+function [objFun] = objective(x)
+%generate geometry
+
+[filename,iter]=aeromodule(x);
+
+%Take design variables into a form the code likes
+[S,X,Z,dih]=DesignToSXZ(x);
+
+%Get iteration number
+
+% iter=getIteration();
+% iter = 1; %/\/\/\/\
+
+iter = 1;
+
+massfile=sprintf('mass_%.0f.mass',iter);
+
+%call avl
+outname=AVLcall(filename,massfile,'w.run',iter);
+
+%read data
+
+[CdCl, CD0, s, k, M]=ReadOutput(outname,x);
+
+
+% objFun = CdCl; %- this will be old I reckon
+
+%% Performance part of objective function
+%load into function Mach, Alt, Wing Area, Zero lift Drag, Induced Drag
+%Outputs Fuel Weight and Fuel Volume - NOTE Fuel Volume used for
+%constraints
+
+%Example inputs
+
+Alt = 38000;
+
+[Mf, Vf] = FuelMassEst(M,Alt,s,CD0,k);
+
+%%
+
+%Joe's Parameters
+L=20;
+N = 3; %Total no. of segment sides to make Wing modules 
+Np = 2; %No. of segment sides dedicated to Passenger Cabin modules
+
+%Establish Weights of BWB components
+Struc_Mass = 131375; %weight of wing structures
+Payload_Mass = 105160; %weight of Passengers
+
+    
+
+%Generate AVL Aero Input file
+[filename,At]=AVLgen(S,Z,dih,X,iter);
+
+%filename = AVLgen(L,N,S,X,iter,alpha);
+[massfilepath, Area_Pass, Vol_Fuel, Fuel_Mass] = MassDist(Np, iter, Struc_Mass, Payload_Mass); %Uses MassPoints & Foil_Integral functions to determine Mass distribution of Segments using previously made .avl file
+
+
+%%
+
+% iterUpdate;
+
+objFun = Mf;  %Load fuel mass as objective function
+
+end
---
+++ main_GA.m
@@ -0,0 +1,59 @@
+%////////MAIN FILE - RUN THIS/////////
+fclose('all');
+close all
+clear all
+
+echo off all
+tstart = tic;
+%set iteration file-workaround
+% filename='iteration.csv';
+% ifile=fopen(filename,'w');
+% fprintf(ifile,'1');
+% 
+% fclose(ifile);
+    
+%Define x0 - Start Point
+%And Upper/Lower Bounds, format:
+%Scale  Root/mid/tip%
+%X      mid/tip
+%Z      mid/tip
+alpha = 0;
+target_cmtot = -0.01;
+alpha_tol = 0.01; % 1% tolerance from target Cm
+
+%x0=[50 30 15 30 40 10 10];
+x0=[40 20 10 20 40 40 40];
+LB=[20 15 10 0 0 10 10];
+UB=[72 72 72 72 72 35 40];
+
+%Write AVL case file and get filename, constraint values
+[filename,iter,At]=aeromodule(x0);
+
+%Define Objective Function
+objFun=@(x)objectiveGA(x);
+
+%Define solver options - Active set Algorithm
+% options = optimoptions('fmincon','Algorithm','active-set','Display','iter-detailed','FiniteDifferenceStepSize',2,'FunctionTolerance',1e-3,'StepTolerance',1e-7,'PlotFcns',@optimplotfval,'MaxIterations',30);
+
+%Interior Point Algorithm
+options = optimoptions('ga','Display','iter','FunctionTolerance',1e-3,'PlotFcn',@gaplotbestf);
+
+
+%Run Optimisation
+[X,fval,exitflag,output,population,scores]=ga(objFun,7,[],[],[],[],LB,UB,@constraintsGA,options);   
+
+%Generate Final AVL case file
+%[filename, iter]=aeromodule(X)
+
+%AVLcall(filename,'mass_1.avl','w.run',iter);
+fclose('all');
+
+%% Output geometry and save file
+% saveas(gcf,'Step_IP2')
+
+vis3D(X)
+% save('J_IP2.mat','J')
+% save('Details_IP2.mat','OUTPUT')
+% save('GeometryOpt_IP2.mat','X')
+
+tEnd = toc(tstart)
